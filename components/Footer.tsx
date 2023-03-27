eedca6fa801647cb56976c96925117fa977dec0f
Diana Zawislak <diana.zawislak@icloud.com>
2023-03-27 17:00:38 -0400

    footer edit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,14 +6,14 @@ export default function Footer() {
   return (
     <MDBFooter bgColor='light' className='text-center text-lg-left'>
       <div className='text-center p-3 mt-9' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-      &copy; {new Date().getFullYear()} Copyright:{'  Diana Zawislak   &#39;'}
-</div>
-
-        <div className='pl-4'>
+        &copy; {new Date().getFullYear()} Copyright:&nbsp;Diana Zawislak&apos;s image generator
+      </div>
+      <div className='pl-4'>
         <a className='text-dark' href='https://www.linkedin.com/in/dianazawislak1980/'>
-          Let's Connect 
+          Let&apos;s Connect 
         </a>
       </div>
     </MDBFooter>
   );
 }
+
